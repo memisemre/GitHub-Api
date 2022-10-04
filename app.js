8339f265a038f1e61d2e3af90ef34cf621c30cd7
memisemre <dev.emrememis@gmail.com>
2022-10-04 11:52:31 +0300

    Change the Design

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ function dataCheck (){
     .then(response=>response.json())
     .then(function(data){
         for(const [key,value] of Object.entries(data)){
-            document.querySelector('.repo-container').innerHTML += `<div class="repo-area">
+            document.querySelector('.repo-areas').innerHTML += `<div class="repo-area">
             <h1>${value.name}</h1>
             <div class="links">
                 <div class="github-link">
-                    <a href="${value.html_url}" target="_blank"">GitHub</a>
+                    <a href="${value.html_url}">GitHub</a>
                 </div>
                 <div class="page-link">
-                    <a href="https://memisemre.github.io/${value.name}/" target="_blank">Page Link</a>
+                    <a href="https://memisemre.github.io/${value.name}/">GitHub Page</a>
                 </div>
-             </div>
+                <div class="latest-commits">
+                    <a href="#">Latest Commit's</a>
+                </div>
+            </div>
         </div>`
         }
     })
 }
-dataCheck()
+dataCheck();
